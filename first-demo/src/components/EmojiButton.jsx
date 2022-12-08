e9071a6863c196838d15ad6aa35cf0267b18504c
CalvinP4 <calvincp28@gmail.com>
2022-12-08 21:04:32 +0530

    [Part 1] Web demo - First react component
    - Explain what are components
    - Explain class vs function components
    - Explain JSX

---
+++ first-demo/src/components/EmojiButton.jsx
@@ -0,0 +1,12 @@
+export default function EmojiButton() {
+    return (
+        <button>
+            <img 
+                src="laugh.png"
+                width={100}
+                height={100}
+                alt="Emoji button"
+            />
+        </button>
+    );
+}
