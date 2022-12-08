38118fc9c993b0e993093f6485ef9d95d26340c4
CalvinP4 <calvincp28@gmail.com>
2022-12-08 22:23:38 +0530

    [Part 1] Web demo - Wire up the state
    - Arrow and regular functions are the same thing
    - React only updates the DOM tree of the element whose state has changed

--- first-demo/src/App.js
+++ first-demo/src/App.js
@@ -4,26 +4,30 @@ import Display from "./components/Display";
 import EmojiButton from "./components/EmojiButton";
 
 function App() {
-
-  const [laughCount, setLaughCount] = useState(2);
+  const [laughCount, setLaughCount] = useState(0);
   const [winkCount, setWinkCount] = useState(0);
 
+  // arrow function
+  const incrementLaughCount = () => {
+    setLaughCount(laughCount + 1);
+  };
 
-  const printHello = () => {
-    console.log("Hello");
-}
+  // regular JS function
+  function incrementWinkCount() {
+    setWinkCount(winkCount + 1);
+  }
 
   return (
     <>
-      <EmojiButton btnImage="laugh.png" onClick={printHello} />
+      <EmojiButton btnImage="laugh.png" onClick={incrementLaughCount} />
       <Display
         text={`I have laughed ${laughCount} times`}
         backgroundColor="red"
         textColor="white"
       />
-      <EmojiButton btnImage="wink.png" onClick={printHello} />
+      <EmojiButton btnImage="wink.png" onClick={incrementWinkCount} />
       <Display
-        text={`I have laughed ${winkCount} times`}
+        text={`I have winked ${winkCount} times`}
         backgroundColor="green"
         textColor="yellow"
       />
