17cddbd6df5380cf8fe406327bfeea9cc7dedbae
CalvinP4 <calvincp28@gmail.com>
2022-12-08 22:14:00 +0530

    [Part 1] Web demo - Pass function as prop
    - Explain how functions are also a type of data
    - Note we are passing a reference of the function to the component and not invoking it

--- first-demo/src/App.js
+++ first-demo/src/App.js
@@ -3,15 +3,20 @@ import Display from "./components/Display";
 import EmojiButton from "./components/EmojiButton";
 
 function App() {
+
+  const printHello = () => {
+    console.log("Hello");
+}
+
   return (
     <>
-      <EmojiButton btnImage="laugh.png" />
+      <EmojiButton btnImage="laugh.png" onClick={printHello} />
       <Display
         text="I have laughed 5 times"
         backgroundColor="red"
         textColor="white"
       />
-      <EmojiButton btnImage="wink.png" />
+      <EmojiButton btnImage="wink.png" onClick={printHello} />
       <Display
         text="I have winked 5 times"
         backgroundColor="green"
--- first-demo/src/components/EmojiButton.jsx
+++ first-demo/src/components/EmojiButton.jsx
@@ -1,20 +1,7 @@
-export default function EmojiButton({btnImage}) {
-
-    const printHello = () => {
-        console.log("Hello");
-    }
-
-
-    return (
-        <button
-            onClick={printHello}
-        >
-            <img 
-                src={btnImage}
-                width={100}
-                height={100}
-                alt="Emoji button"
-            />
-        </button>
-    );
+export default function EmojiButton({ btnImage, onClick }) {
+  return (
+    <button onClick={onClick}>
+      <img src={btnImage} width={100} height={100} alt="Emoji button" />
+    </button>
+  );
 }
