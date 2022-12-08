de140f5b0251ee31641152410073e8b839bd9926
CalvinP4 <calvincp28@gmail.com>
2022-12-08 21:19:37 +0530

    [Part 1] Web demo - Power of components
    - Components are reusable

--- first-demo/src/App.js
+++ first-demo/src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import Display from './components/Display';
-import EmojiButton from './components/EmojiButton';
+import "./App.css";
+import Display from "./components/Display";
+import EmojiButton from "./components/EmojiButton";
 
 function App() {
   return (
     <>
-    <EmojiButton 
-      btnImage="laugh.png"
-    />
-    <Display 
-      text="I have been clicked 5 times"
-      backgroundColor="red"
-      textColor="white"
-    />
+      <EmojiButton btnImage="laugh.png" />
+      <Display
+        text="I have laughed 5 times"
+        backgroundColor="red"
+        textColor="white"
+      />
+      <EmojiButton btnImage="wink.png" />
+      <Display
+        text="I have winked 5 times"
+        backgroundColor="green"
+        textColor="yellow"
+      />
     </>
   );
 }
