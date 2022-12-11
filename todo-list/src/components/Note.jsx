79fe10a1060fa24a257c5b63f82a9679bdb2d076
CalvinP4 <calvincp28@gmail.com>
2022-12-11 09:49:40 +0530

    [Part 3]Todo list - Note icon row
    - Explain flex
    - Replace TouchableHighlight with TouchableOpacity

--- todo-list/src/components/Note.jsx
+++ todo-list/src/components/Note.jsx
@@ -1,28 +1,42 @@
 import React from "react";
-import { Text, TouchableHighlight, View, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Note() {
-    return (
-        <View style={styles.note}>
-            <Text style={styles.noteHeader}>Title</Text>
-            <Text>This is some content</Text>
-        </View>
-    );
+  return (
+    <View style={styles.note}>
+      <Text style={styles.noteHeader}>Title</Text>
+      <Text>This is some content</Text>
+      <View  style={styles.buttonRow}>
+        <TouchableOpacity>
+          <Ionicons name="trash-bin-outline" size={32} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="trash-bin-outline" size={32} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="trash-bin-outline" size={32} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    note: {
-        backgroundColor: "#C0BBDE",
-        borderRadius: 10,
-        height: 150,
-        width: 360,
-        padding: 8,
-        margin: 5,
-      },
-      noteHeader: {
-        fontSize: 20,
-        fontWeight: "bold",
-      },
+  note: {
+    backgroundColor: "#C0BBDE",
+    borderRadius: 10,
+    height: 150,
+    width: 360,
+    padding: 8,
+    margin: 5,
+  },
+  noteHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    marginTop: 32,
+  },
 });
