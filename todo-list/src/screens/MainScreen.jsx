d68c691867062f8770ba775a169db95e7b662f01
CalvinP4 <calvincp28@gmail.com>
2022-12-11 10:19:30 +0530

    [Part 3] Todo list - Finishing up Main screen
    - Add create button
    - Wrap it in a fragment

--- todo-list/src/screens/MainScreen.jsx
+++ todo-list/src/screens/MainScreen.jsx
@@ -1,16 +1,20 @@
 import Note from "../components/Note";
 import { ScrollView } from "react-native";
+import CreateButton from "../components/CreateButton";
 
 export default function MainScreen() {
   return (
-    <ScrollView style={{ padding: 4, alignSelf: "center", marginBottom: 4 }}>
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-    </ScrollView>
+    <>
+      <ScrollView style={{ padding: 4, alignSelf: "center", marginBottom: 4 }}>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </ScrollView>
+      <CreateButton />
+    </>
   );
 }
