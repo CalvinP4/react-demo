be8a5ff095298775cb9beb0b4f36a590e68254b1
CalvinP4 <calvincp28@gmail.com>
2022-12-11 09:35:21 +0530

    [Part 3]Todo list - Icons
    - Ionicons is created on top of a library called vector-icons
    - Show list of icons on website

--- todo-list/src/components/CreateButton.jsx
+++ todo-list/src/components/CreateButton.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function CreateButton() {
   return (
-    <TouchableOpacity
-      style={styles.createButton}
-    >
-      <Text style={styles.icon}>+</Text>
+    <TouchableOpacity style={styles.createButton}>
+      <Ionicons name="add" size={32} color="white" />
     </TouchableOpacity>
   );
 }
 
-
 const styles = StyleSheet.create({
-    createButton: {
-        elevation: 4,
-        width: 80,
-        height: 80,
-        borderRadius: 80,
-        backgroundColor: "#401E5B",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
-        bottom: 8,
-        right: 4,
-      },
-      icon: {
-        fontSize: 50, 
-        color: "white"
-      }
+  createButton: {
+    elevation: 4,
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    backgroundColor: "#401E5B",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 8,
+    right: 4,
+  },
+  icon: {
+    fontSize: 50,
+    color: "white",
+  },
 });
