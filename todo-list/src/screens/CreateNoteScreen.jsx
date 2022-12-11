6e63014ed6b1f4a6b95a181dd98674eaeaae35ce
CalvinP4 <calvincp28@gmail.com>
2022-12-11 11:25:01 +0530

    [Part 3]Todo list - form validation
    - How state is used for validation

--- todo-list/src/screens/CreateNoteScreen.jsx
+++ todo-list/src/screens/CreateNoteScreen.jsx
@@ -8,53 +8,64 @@ import {
 } from "react-native";
 
 export default function CreateNoteScreen() {
-    return (
-        <View>
-            <TextInput 
-                style={styles.title}
-                placeholder="Title"
-            />
-            <TextInput 
-                multiline
-                style={styles.content}
-                placeholder="Content"
-                numberOfLines={20}
-            />
-            <TouchableOpacity style={styles.buttonEnabled}>
-                <Text style={styles.buttonText}>Add note</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  const [title, onChangeTitle] = React.useState("");
+  const [content, onChangeContent] = React.useState("");
+
+  return (
+    <View>
+      <TextInput
+        style={styles.title}
+        placeholder="Title"
+        onChangeText={onChangeTitle}
+      />
+      <TextInput
+        multiline
+        style={styles.content}
+        placeholder="Content"
+        numberOfLines={20}
+        onChangeText={onChangeContent}
+      />
+      <TouchableOpacity
+        style={
+          title === "" || content === ""
+            ? styles.buttonDisabled
+            : styles.buttonEnabled
+        }
+        disabled={title === "" || content === ""}
+      >
+        <Text style={styles.buttonText}>Add note</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: {
-      height: 40,
-      margin: 10,
-      borderWidth: 1,
-      borderColor: "#C0BBDE",
-      padding: 4,
-    },
-    content: {
-      borderWidth: 1,
-      borderColor: "#C0BBDE",
-      margin: 10,
-      padding: 4,
-    },
-    buttonDisabled: {
-      alignItems: "center",
-      backgroundColor: "#D1BCE0",
-      padding: 10,
-      margin: 10,
-    },
-    buttonEnabled: {
-      alignItems: "center",
-      backgroundColor: "#8225C4",
-      padding: 10,
-      margin: 10,
-    },
-    buttonText: {
-      color: "white",
-    },
-  });
-  
+  title: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#C0BBDE",
+    padding: 4,
+  },
+  content: {
+    borderWidth: 1,
+    borderColor: "#C0BBDE",
+    margin: 10,
+    padding: 4,
+  },
+  buttonDisabled: {
+    alignItems: "center",
+    backgroundColor: "#D1BCE0",
+    padding: 10,
+    margin: 10,
+  },
+  buttonEnabled: {
+    alignItems: "center",
+    backgroundColor: "#8225C4",
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+});
